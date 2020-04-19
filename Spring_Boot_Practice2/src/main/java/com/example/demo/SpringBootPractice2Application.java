0f35933f6762d64a9c7aaff56b7203f5e3404c81
Gauttam Sonkamble <46339795+Gauttamsonkamble@users.noreply.github.com>
2020-04-19 15:18:40 +0530

    Add files via upload

---
+++ Spring_Boot_Practice2/src/main/java/com/example/demo/SpringBootPractice2Application.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPractice2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPractice2Application.class, args);
+		
+		System.out.println("Hello");
+	}
+
+}
